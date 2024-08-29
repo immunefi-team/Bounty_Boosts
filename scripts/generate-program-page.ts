d50bf1b2905e1dcd73350d4857d42a988aad846a
yovanylg <yovany.luis@immunefi.com>
2024-08-28 19:15:19 -0600

    fixes ordering of Severities in gnerate-program-pages

--- scripts/generate-program-page.ts
+++ scripts/generate-program-page.ts
@@ -1,55 +1,9 @@
 import fs from "fs";
 import path from "path";
 
-function searchContent(content: string, searchString: string) {
-  // Read the file content
-  const lines = content.split("\n");
-  let result: string | null = null;
-  for (const line of lines) {
-    const regex = new RegExp(`${searchString}\\s+(.+)`, "i");
-    const match = line.match(regex);
-
-    if (match) {
-      result = match[1]; // Capture the desired part
-      break;
-    }
-  }
-  return result;
-}
-
-type ReportMetadata = {
-  filePath: string;
-  reportType: string;
-  reportSeverity: string;
-};
-
-function getReportMetadata(filePath: string): ReportMetadata | null {
-  try {
-    const content = fs.readFileSync(filePath, "utf-8");
-    const reportType = searchContent(content, "Report Type:");
-    const reportSeverity = searchContent(content, "Report severity:");
-
-    if (!reportType) {
-      console.error(`Report Type not found in file: ${filePath}`);
-    }
-    if (!reportSeverity) {
-      console.error(`Report Severity not found in file: ${filePath}`);
-    }
-
-    return {
-      filePath,
-      reportType: reportType ?? "",
-      reportSeverity: reportSeverity ?? "",
-    };
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(`Error reading file: ${error.message}`);
-    }
-    console.error(`Error reading file: ${filePath}`);
-  }
-
-  return null;
-}
+/**
+ * File related functions
+ */
 
 type ProgramDirectory = {
   fullPath: string;
@@ -86,25 +40,98 @@ function listMarkdownFiles(dir: string, ignore: string[] = []) {
   return results;
 }
 
-function groupReportsByType(reports: (ReportMetadata | null)[]) {
-  const byReportType = reports.reduce((acc, report) => {
-    if (report) {
-      acc[report.reportType] = acc[report.reportType] ?? [];
-      acc[report.reportType].push(report);
+function searchContent(content: string, searchString: string) {
+  // Read the file content
+  const lines = content.split("\n");
+  let result: string | null = null;
+  for (const line of lines) {
+    const regex = new RegExp(`${searchString}\\s+(.+)`, "i");
+    const match = line.match(regex);
+
+    if (match) {
+      result = match[1]; // Capture the desired part
+      break;
     }
+  }
+  return result;
+}
+
+/**
+ * Report related functions
+ */
+
+enum Severity {
+  Critical = "Critical",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Insight = "Insight",
+}
+
+const SeverityOrder = {
+  [Severity.Insight]: 1,
+  [Severity.Low]: 2,
+  [Severity.Medium]: 3,
+  [Severity.High]: 4,
+  [Severity.Critical]: 5,
+};
+
+type ReportMetadata = {
+  filePath: string;
+  reportType: string;
+  reportSeverity: Severity;
+};
+
+function isReportSeverity(severity: string): severity is Severity {
+  return Object.values(Severity).includes(severity as Severity);
+}
+
+function getReportMetadata(filePath: string): ReportMetadata | null {
+  try {
+    const content = fs.readFileSync(filePath, "utf-8");
+    const reportType = searchContent(content, "Report Type:");
+    const reportSeverity = searchContent(content, "Report severity:");
+
+    if (!reportType) {
+      console.error(`Report Type not found in file: ${filePath}`);
+    }
+
+    if (!reportSeverity || !isReportSeverity(reportSeverity)) {
+      console.error(`Report Severity not found in file: ${filePath}`);
+    }
+
+    return {
+      filePath,
+      reportType: reportType ?? "",
+      reportSeverity: reportSeverity as Severity,
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`Error reading file: ${error.message}`);
+    }
+    console.error(`Error reading file: ${filePath}`);
+  }
+
+  return null;
+}
+
+function groupReportsByType(reports: ReportMetadata[]) {
+  const byReportType = reports.reduce((acc, report) => {
+    acc[report.reportType] = acc[report.reportType] ?? [];
+    acc[report.reportType].push(report);
     return acc;
   }, {} as Record<string, ReportMetadata[]>);
+
   return byReportType;
 }
 
-function groupReportsBySeverity(reports: (ReportMetadata | null)[]) {
+function groupReportsBySeverity(reports: ReportMetadata[]) {
   const reportsBySeverity = reports.reduce((acc, report) => {
-    if (report) {
-      acc[report.reportSeverity] = acc[report.reportSeverity] ?? [];
-      acc[report.reportSeverity].push(report);
-    }
+    acc[report.reportSeverity] = acc[report.reportSeverity] ?? [];
+    acc[report.reportSeverity].push(report);
     return acc;
-  }, {} as Record<string, ReportMetadata[]>);
+  }, {} as Record<Severity, ReportMetadata[]>);
+
   return reportsBySeverity;
 }
 
@@ -139,16 +166,18 @@ function sluggify(title: string) {
     .trim() // Remove leading and trailing spaces
     .replace(/[\s]+/g, "-") // Replace spaces with hyphens
     .replace(/[\/]+/g, "-") // Replace forward slashes with hyphens
-    .replace(/[^\w\-]+/g, "") // Remove all non-word characters (except hyphens)
+    .replace(/[^\w\-]+/g, ""); // Remove all non-word characters (except hyphens)
 }
 
 function generateProgramReadme(directory: ProgramDirectory) {
   console.log("Processing: ", directory.name);
   const markdownFiles = listMarkdownFiles(directory.fullPath, ["README.md"]);
-  const reports = markdownFiles.map((file) => getReportMetadata(file));
+  const reports = markdownFiles
+    .map((file) => getReportMetadata(file))
+    .filter((report): report is ReportMetadata => report !== null);
 
   const reportsByType = groupReportsByType(reports);
-  const reportsBySeverity = groupReportsBySeverity(reports);
+  const reportsBySeverity = groupReportsBySeverity(reports)
   const contentByType = Object.entries(reportsByType).map(
     ([reportType, reports]) =>
       getReportsGroupContent({
@@ -156,7 +185,13 @@ function generateProgramReadme(directory: ProgramDirectory) {
         reports,
       })
   );
-  const contentBySeverity = Object.entries(reportsBySeverity).map(
+  const reportsBySeverityEntries = Object.entries(reportsBySeverity) as [Severity, ReportMetadata[]][];
+  reportsBySeverityEntries.sort((entryA, entryB) => {
+    const [severityA, _reportsA] = entryA;
+    const [severityB, _reportsB] = entryB;
+    return SeverityOrder[severityB] - SeverityOrder[severityA];
+  });
+  const contentBySeverity = reportsBySeverityEntries.map(
     ([reportSeverity, reports]) =>
       getReportsGroupContent({
         groupName: reportSeverity,
